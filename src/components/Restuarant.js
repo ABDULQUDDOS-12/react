189d8299383d1373f6cd26625ed178ee47bc3a88
Abdul quddos <abdulquddoshafiz@gmail.com>
2023-03-23 17:27:17 +0500

    react

---
+++ src/components/Restuarant.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./style.css"
+const myStyle={color:"red"}
+const Restuarant = () => {
+  return (
+    <>
+    <div className='card-container'>
+        <div className="card">
+            <div className="card-body">
+            <span className="card-number card-circle subtle">1</span>
+            <span className="card-author subtle" style={myStyle}>Breakfast</span>
+
+            </div>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Restuarant
