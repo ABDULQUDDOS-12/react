490c88748a0bb7077dad6d2b0c3f18ef8a1a9520
Abdul quddos <abdulquddoshafiz@gmail.com>
2023-03-24 14:44:13 +0500

    first project

--- src/components/Restuarant.js
+++ src/components/Restuarant.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import "./style.css"
+import React from 'react';
+import "./style.css";
+import image from "./Korma.jpg";
 const myStyle={color:"red"}
 const Restuarant = () => {
   return (
@@ -9,8 +10,15 @@ const Restuarant = () => {
             <div className="card-body">
             <span className="card-number card-circle subtle">1</span>
             <span className="card-author subtle" style={myStyle}>Breakfast</span>
-
+            <h2 className="card-title">chicken korma</h2>
+            <span className="card-description subtle">
+              I love Chicken korma and this is my favourite dish 
+              amongst alls so if i can choose many so i will choose Chicken
+              korma, karahi, kheer, shwarma
+            </span>
+            <div className="card-read">Read</div>
             </div>
+            <img src={image} alt="images" className='card-media'/>
         </div>
     </div>
     </>
