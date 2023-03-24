a93d9767d81c3068e86d7e22c9f0a297122aa4d3
Abdul quddos <abdulquddoshafiz@gmail.com>
2023-03-24 17:14:24 +0500

    first project

--- src/components/Restuarant.js
+++ src/components/Restuarant.js
@@ -2,14 +2,33 @@ import React, { useState } from 'react';
 import "./style.css";
 import Menu from './menuApi';
 import MenuCard from './menuCard';
+import Navbar from './Navbar';
+const uniqueList=[...new Set(Menu.map((curElem)=>{
+  return curElem.category
+})
+),"All",
+]
+console.log(uniqueList)
+
 const Restuarant = () => {
   //usestate hooks
   //array of two variable 1st state variable, used for data management
   //if we want to use a component many times then we need props
   const [menuData,setmenuData] = useState(Menu)
+  const [menuList,setmenuList] = useState(uniqueList)
+  const filterItem =(category)=>{
+    if(category==="All"){
+      setmenuData(Menu)
+      return
+    }
+     const updatedList = Menu.filter((curElem)=>{
+         return curElem.category === category
+     })
+     setmenuData(updatedList)
+  }
 return (
     <>
-      {/* we use props after menucard */}
+      <Navbar filterItem={filterItem} menuList={menuList} />
       <MenuCard menuData={menuData} />
     </>
   )
