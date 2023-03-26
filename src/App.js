4e1c78272cbc28e5dfb123980b2350731b3d87b3
Abdul quddos <abdulquddoshafiz@gmail.com>
2023-03-26 11:48:04 +0500

    font-awesome

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react'
 // import Restuarant from './components/Restuarant'
 // import UseState from './components/hooks/useState'
 // import UseEffect from './components/hooks/useEffect'
-import UseReducer from './components/hooks/useState'
+// import UseReducer from './components/hooks/useState'
+import Todo from './components/todoReact/todo'
 const App = () => {
   return (
    
     <div>
-      <UseReducer/>
+      <Todo/>
       {/* <UseEffect/> */}
        {/* <UseState/> */}
        {/* <Restuarant/> */}
--- src/components/todoReact/todo.js
+++ src/components/todoReact/todo.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import "./style.css"
-const todo = () => {
+const Todo = () => {
   return (
     <>
     <div className='main-div'>
+        <div className="child-div">
         <figure>
-            <img src="" alt="" />
+            <img src="./images/todo.svg" alt="todologo" />
+        <figcaption>Add your list here ✌</figcaption>
         </figure>
+        <div className="addItems">
+        <input type="text" name="" id="" placeholder='✍ add item' className='form-control'/>
+        <FontAwesomeIcon icon="fa-solid fa-plus add-btn" />
+        </div>
+        </div>
     </div>
   </>
   )
 }
 
-export default todo
+export default Todo
